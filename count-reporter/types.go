dfe20afebb89edb021fb09912745ac3f157c421c
Wesley Smith <me@wesjd.net>
2018-06-30 14:31:47 -0400

    apply config fixes to all systems

--- count-reporter/types.go
+++ count-reporter/types.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/WesJD/proxy-scraper/config"
+import (
+	"github.com/WesJD/proxy-scraper/config"
+	"encoding/json"
+)
 
 type Configuration struct {
 	Sql config.SQLDatabaseConfiguration `json:"sql"`
 	Influx config.InfluxDatabaseConfiguration `json:"influx"`
 	Reporting config.StatisticsReportingConfiguration `json:"reporting"`
+}
+
+func (config Configuration) Read(data []byte) (out interface{}, err error) {
+	ret := Configuration{}
+	err = json.Unmarshal(data, &ret)
+	if err != nil {
+		return
+	}
+	out = ret
+	return
 }
--- scraper/types.go
+++ scraper/types.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/WesJD/proxy-scraper/config"
+import (
+	"github.com/WesJD/proxy-scraper/config"
+	"encoding/json"
+)
 
 type Configuration struct {
 	Sql config.SQLDatabaseConfiguration `json:"sql"`
 	Checking config.ProxyCheckerConfiguration `json:"checking"`
 	HttpClient config.HttpClientDefaultsConfiguration `json:"client"`
+}
+
+func (config Configuration) Read(data []byte) (out interface{}, err error) {
+	ret := Configuration{}
+	err = json.Unmarshal(data, &ret)
+	if err != nil {
+		return
+	}
+	out = ret
+	return
 }
